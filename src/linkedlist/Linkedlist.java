f9e4667badc100d0ff4746c271afd0c4fe9110d9
himanshusingh11 <himanshu.singh_iot18@gla.ac.in>
2020-02-19 00:06:01 +0530

    Initial commit

---
+++ src/linkedlist/Linkedlist.java
@@ -0,0 +1,147 @@
+package linkedlist;
+
+public class Linkedlist {
+    private Node head;
+    private int size;
+
+    public Linkedlist() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    public Linkedlist(Node head, int size) {
+        this.head = head;
+        this.size = size;
+    }
+
+
+    public boolean isEmpty() {
+        if (head == null) {
+            return true;
+        } else
+            return false;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void insertAtFirst(int value) {
+        Node n = new Node();
+        n.setData(value);
+        n.setNext(head);// it will works in both the case empty or non-empty
+        head = n;
+        size++;
+    }
+
+    public void insertAtLast(int value) {
+        Node n = new Node();
+        Node temp;
+        n.setData(value);
+        temp = head;
+        if (temp == null) {
+            head = n;
+        } else {
+
+            while (temp.getNext()!= null) {
+                temp = temp.getNext();
+                temp.setNext(n);
+
+            }
+        }
+        size++;
+    }
+
+    public void insertAtPos(int val, int pos) {
+        if (pos == 1) {
+            insertAtFirst(val);
+        } else if (pos == size + 1) {
+            insertAtLast(val);
+        } else if (pos > 1 && pos <= size) {
+            Node n = new Node(val, null);
+            Node temp;
+            temp = head;//temp will point to the first node
+            for (int i = 1; i < pos - 1; i++) {
+                temp = temp.getNext();
+                n.setNext(temp.getNext());
+                temp.setNext(n);
+                size++;
+            }
+
+        } else {
+            System.out.println("insertion not possible at the given position " + pos);
+        }
+    }
+
+    public void deleteFirst() {
+        if (head == null) {
+            System.out.println("list is empty");
+
+        } else {
+            head = head.getNext();
+            size--;
+        }
+    }
+
+    public void deleteLast() {
+        if (head == null) {
+            System.out.println("list is empty");
+
+        } else if (size == 1) {
+            head = null;
+            size--;
+
+        } else {
+            Node temp;
+            temp = head;
+            for (int i = 0; i < size - 1; i++) {
+                temp = temp.getNext();
+                temp.setNext(null);
+                size--;
+            }
+        }
+    }
+
+    public void deleteAtPos(int pos) {
+        if (head == null) {
+            System.out.println("list is empty");
+
+        } else if (pos < 1 || pos > size) {
+
+            System.out.println("Invalid position");
+        } else if (pos == 1) {
+            deleteFirst();
+        } else if (pos == size) {
+            deleteLast();
+        } else {
+            Node temp, temp1;
+            temp = head;
+            for (int i = 0; i < pos - 1; i++) {
+                temp = temp.getNext();
+                temp1 = temp.getNext();
+                temp.setNext(temp1.getNext());
+                size--;
+            }
+        }
+
+    }
+
+
+
+   public  void printList(){
+        System.out.println("mylist:");
+        if(head==null){
+            System.out.println("empty list");
+        }
+        else{
+            Node temp=head;
+            while(temp!=null){
+                System.out.print(temp.getData()+"--->");
+                temp=temp.getNext();
+            }
+            System.out.println("null");
+
+        }
+    }
+
+}
---
+++ src/linkedlist/Main.java
@@ -0,0 +1,67 @@
+package linkedlist;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Linkedlist linkedlist = new Linkedlist();
+        boolean flag = true;
+        while (flag) {
+            System.out.println(" ");
+            System.out.println("1. Add item to the list at start");
+            System.out.println("2. Add item to the list at Last");
+            System.out.println("3. Add item to the list at position");
+            System.out.println("4.  Delete First item from the list");
+            System.out.println("5. Delete Last item from the list ");
+            System.out.println("6. Delete  item from the list with given position ");
+            System.out.println("7. Print List ");
+            System.out.println("8. Exit");
+            System.out.println("Enter your choice");
+            System.out.println();
+            int choice = scanner.nextInt();
+            int position;
+            int val;
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter value of list Item");
+                    val = scanner.nextInt();
+                    linkedlist.insertAtFirst(val);
+                    break;
+                case 2:
+                    System.out.println("Enter value of list Item");
+                    val = scanner.nextInt();
+                    linkedlist.insertAtLast(val);
+                    break;
+                case 3:
+                    System.out.println("Enter position");
+                    position = scanner.nextInt();
+                    System.out.println("Enter value of list Item");
+                    val = scanner.nextInt();
+                    linkedlist.insertAtPos(position, val);
+                    break;
+                case 4:
+                    linkedlist.deleteFirst();
+                    break;
+                case 5:
+                    linkedlist.deleteLast();
+                    break;
+                case 6:
+                    System.out.println("Enter position");
+                    position = scanner.nextInt();
+                    linkedlist.deleteAtPos(position);
+                    break;
+                case 7:
+                    linkedlist.printList();
+                   break;
+                case 8:
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice ");
+
+
+            }
+        }
+    }
+}
